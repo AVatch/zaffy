ac0b9df0cc6956dca881897fa7a2d90250d80aec
Zafrir Schop <zafrir.schop@gmail.com>
2015-03-06 21:01:48 -0500

    test image

---
+++ test.py
@@ -0,0 +1,23 @@
+from skimage import data, io, segmentation, color
+from skimage.future import graph
+from matplotlib import pyplot as plt
+from scipy import misc
+
+# img = misc.imread('test.png')
+# plt.imshow(lena)
+# plt.show()
+
+img = data.coffee()
+
+labels1 = segmentation.slic(img, compactness=30, n_segments=400)
+out1 = color.label2rgb(labels1, img, kind='avg')
+
+g = graph.rag_mean_color(img, labels1, mode='similarity')
+labels2 = graph.cut_normalized(labels1, g)
+out2 = color.label2rgb(labels2, img, kind='avg')
+
+plt.figure()
+io.imshow(out1)
+plt.figure()
+io.imshow(out2)
+io.show()
