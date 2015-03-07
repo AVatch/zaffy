f592cec1b2c661a027d8b238a0b45e36957625b0
AntonKueltz <kueltz.anton@gmail.com>
2015-03-07 16:50:32 -0500

    reddit scraing done

--- zaffy_api/scrapers/reddit.py
+++ zaffy_api/scrapers/reddit.py
@@ -15,5 +15,39 @@ def pull_reddit_listing(listing='popular'):
         print e
 
 
+def isimg(link):
+    ex = ['.png', '.jpg']
+    for e in ex:
+        if link[-4:] == e:
+            return True
+
+    if 'imgur' in link:
+        return True
+
+    return False
+
+
+def twitter():
+    pass
+
+
+def reddit():
+    pairs = []
+
+    try:
+        page = urllib2.urlopen('http://www.reddit.com/r/all/top.json?limit=10')
+        data = json.load(page)
+
+        for d in data['data']['children']:
+            title = d['data']['title']
+            link = d['data']['url']
+            img = link if isimg(link) else None
+            pairs.append((title, img))
+
+        return pairs
+
+    except:
+        return None
+
 if __name__ == '__main__':
-    pull_reddit_listing()
+    reddit_data = reddit()
