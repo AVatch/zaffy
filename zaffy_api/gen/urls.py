5d7d0e2216e6ec547c1885826f83c3a2270f7bfe
Adrian Vatchinsky <avatchinsky@outlook.com>
2015-03-07 23:47:03 -0500

    updated urls

--- zaffy_api/gen/urls.py
+++ zaffy_api/gen/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from gen import views
 
@@ -6,3 +7,5 @@ urlpatterns = patterns('',
 
     url(r'^api/v1/gen/zaffy/$', views.gen_zaffy),
 )
+
+urlpatterns += staticfiles_urlpatterns()
